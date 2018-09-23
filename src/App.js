57e40e929ad50844dd66d34645e1cc3366d56b8e
Mo'men Soliman <moumensoliman@gmail.com>
2018-09-23 17:33:04 +0200

    Add components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import PostForm from './components/PostForm';
+import AllPost from './components/AllPost';
 
 class App extends Component {
   render() {
       return (
       <div className="App">
-        <h1>Hello React!</h1>
+        <AllPost />
+        <PostForm />
       </div>
       );
   }
-  }
+}
 export default App;
---
+++ src/components/AllPost.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AllPost = () => <h1>This is All Posts</h1>
+
+export default AllPost;
